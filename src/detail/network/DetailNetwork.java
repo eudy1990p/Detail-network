cfd37e36cdd47f238ecf6723aa2992fdeec7b399
eudy1990p <eudy1990@gmail.com>
2016-12-06 15:21:25 -0400

    inicio

---
+++ src/detail/network/DetailNetwork.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package detail.network;
+
+/**
+ *
+ * @author Eudy
+ */
+public class DetailNetwork {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] arg){
+       View1 v = new View1();
+       v.setVisible(true);
+   }
+    
+}
